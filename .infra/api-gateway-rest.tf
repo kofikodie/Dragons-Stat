fa54052e61edd0331ef783001424c921f69b9853
Kofi Addo <kofi.addo@facile.it>
2022-02-19 20:57:22 +0100

    feat: add mock response for api gateway

--- .infra/api-gateway-rest.tf
+++ .infra/api-gateway-rest.tf
@@ -2,7 +2,7 @@ resource "aws_api_gateway_rest_api" "dragon_api" {
   name = "dragon_api"
   endpoint_configuration {
     types = ["REGIONAL"]
-   }
+  }
 }
 
 resource "aws_api_gateway_resource" "dragons_api_resource" {
@@ -24,4 +24,38 @@ resource "aws_api_gateway_integration" "dragon_api_integration" {
   resource_id = aws_api_gateway_resource.dragons_api_resource.id
   rest_api_id = aws_api_gateway_rest_api.dragon_api.id
   type        = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "dragon_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.dragon_api.id
+  resource_id = aws_api_gateway_resource.dragons_api_resource.id
+  http_method = aws_api_gateway_method.dragon_api_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "dragon_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.dragon_api.id
+  resource_id = aws_api_gateway_resource.dragons_api_resource.id
+  http_method = aws_api_gateway_method.dragon_api_method.http_method
+  status_code = aws_api_gateway_method_response.dragon_response_200.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+resource "aws_api_gateway_model" "dragon_model" {
+  rest_api_id  = aws_api_gateway_rest_api.dragon_api.id
+  name         = "user"
+  description  = "a JSON schema"
+  content_type = "application/json"
+
+  schema = <<EOF
+{
+  "type": "object"
+}
+EOF
 }
