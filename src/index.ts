07568667b484e3a398a575b4d591db0a1eb6d99f
Kofi Addo <kofi.addo@facile.it>
2022-02-25 21:33:56 +0100

    feat: lambda func and serverless deploy

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import AWS from 'aws-sdk'
+import { APIGatewayEvent, ProxyResult } from 'aws-lambda'
 import { StreamingEventStream } from 'aws-sdk/lib/event-stream/event-stream'
 
 const s3 = new AWS.S3({
     region: 'eu-central-1',
 })
 
-export async function readDragons(): Promise<void> {
+exports.handler = async (event: APIGatewayEvent): Promise<ProxyResult> => {
+    return await readDragons(event)
+}
+
+export async function readDragons(event: APIGatewayEvent): Promise<any> {
     const s3Bucket = await s3.listBuckets().promise()
     const s3files = await s3
         .listObjects({ Bucket: 'dragons-stat-bucket' })
         .promise()
 
-    let bucketName: string | undefined = ''
-    let fileName: string | undefined = ''
+    let bucketName: string = ''
+    let fileName: string = ''
+
     s3Bucket?.Buckets?.forEach((bucket: AWS.S3.Bucket) => {
         if (bucket?.Name?.includes('dragons-stat-bucket')) {
             bucketName = bucket.Name
@@ -24,18 +30,47 @@ export async function readDragons(): Promise<void> {
             fileName = file.Key
         }
     })
+
     if (bucketName && fileName) {
-        readDragonsFromS3(bucketName, fileName)
+        const dragonList = await readDragonsFromS3(bucketName, fileName, event)
+        if (typeof dragonList === 'string') {
+            return {
+                statusCode: 200,
+                body: dragonList,
+            }
+        }
+        return {
+            statusCode: 500,
+            body: 'Error while reading from S3',
+        }
     } else {
-        console.log('No bucket or file found')
+        return {
+            statusCode: 500,
+            body: 'No bucket or file found',
+        }
     }
 }
 
-function readDragonsFromS3(bucketName: string, fileName: string): void {
-    const expression = 'select * from s3object s'
+async function readDragonsFromS3(
+    bucketName: string,
+    fileName: string,
+    event: APIGatewayEvent
+): Promise<string | false | Error> {
+    let expression = 'select * from s3object s'
 
-    s3.selectObjectContent(
-        {
+    if (
+        event.queryStringParameters &&
+        event.queryStringParameters.family &&
+        event.queryStringParameters.family !== ''
+    ) {
+        expression =
+            "select * from S3Object[*][*] s where s.family_str =  '" +
+            event['queryStringParameters']['family'] +
+            "'"
+    }
+
+    const result = await s3
+        .selectObjectContent({
             Bucket: bucketName,
             Expression: expression,
             ExpressionType: 'SQL',
@@ -50,29 +85,31 @@ function readDragonsFromS3(bucketName: string, fileName: string): void {
                     RecordDelimiter: ',',
                 },
             },
-        },
-        (err, data) => {
-            if (err) {
-                console.log(err)
-            } else {
-                const eventStream =
-                    data.Payload as StreamingEventStream<AWS.S3.SelectObjectContentEventStream>
-                return handleData(eventStream)
-            }
-        }
-    )
+        })
+        .promise()
+
+    if (result) {
+        const eventStream =
+            result.Payload as StreamingEventStream<AWS.S3.SelectObjectContentEventStream>
+        return await handleData(eventStream)
+    }
+
+    return false
 }
 
-function handleData(
+async function handleData(
     data: StreamingEventStream<AWS.S3.SelectObjectContentEventStream>
-): void {
-    data.on('data', (event: any) => {
-        if (event.Records) {
-            console.log(event.Records.Payload.toString())
-        }
+): Promise<Error | string> {
+    return new Promise((resolve, reject) => {
+        let dataString = ''
+        data.on('data', (event: any) => {
+            dataString += event.Records?.Payload?.toString()
+        })
+        data.on('end', () => {
+            resolve(dataString)
+        })
+        data.on('error', (error: Error) => {
+            reject(error)
+        })
     })
 }
-
-;(async () => {
-    readDragons()
-})()
